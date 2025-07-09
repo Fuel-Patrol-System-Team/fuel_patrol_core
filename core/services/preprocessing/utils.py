e3b8d85ee8e297752994e6f211e0ca81406eba98
yshipik <yshipik@yandex.ru>
2025-07-09 14:43:41 +0300

    Fix(calculations): fixes for ratio

--- core/services/preprocessing/utils.py
+++ core/services/preprocessing/utils.py
@@ -143,7 +143,7 @@ def merge(car_data: pd.DataFrame, preprocessed_df: pd.DataFrame):
 
 
 #TODO:save_bad_data
-def fuel_leak_calculate_standart(df_values: pd.DataFrame, norma_rasx_df: pd.DataFrame, DECREASE_INCREASE_RATIO = 0.75, LEAK_LIMIT = 12, SIGMA_LIMIT = 3.5, MULT_STD_MEAN_DIFF = 2.25, FUEL_JUMPS_AMOUNT = 30, LEAK_FACTOR = 0.05, SMALL_SPEED_FACTOR = 2.25, UNREASONABLE_FUEL_LEVEL = 1000 , UNTARIFF_LEVEL = 400, is_save_bad_data = False):
+def fuel_leak_calculate_standart(df_values: pd.DataFrame, norma_rasx_df: pd.DataFrame, FUEL_PURITY_VALUE = 12, DECREASE_INCREASE_RATIO = 0.75, LEAK_LIMIT = 12, SIGMA_LIMIT = 3.5, MULT_STD_MEAN_DIFF = 2.25, FUEL_JUMPS_AMOUNT = 30, LEAK_FACTOR = 0.05, SMALL_SPEED_FACTOR = 2.25, UNREASONABLE_FUEL_LEVEL = 1000 , UNTARIFF_LEVEL = 400, is_save_bad_data = False):
     df_values['is_bad_data_count'] = df_values['count'].le(5)
     df_values['is_bad_data_jitter'] = df_values['jumps'].gt(FUEL_JUMPS_AMOUNT)
     
@@ -234,7 +234,8 @@ def fuel_leak_calculate_standart(df_values: pd.DataFrame, norma_rasx_df: pd.Data
 
     season_result['low_speed'] = season_result['pos_s'].le(season_result['speed_etalon'].div(SMALL_SPEED_FACTOR))
     season_result['is_leak'] = season_result['low_speed'].eq(True) & season_result['is_leak'].eq(True)
-    season_result['is_leak'] = season_result['is_leak'] & season_result['ratio'].gt(DECREASE_INCREASE_RATIO)
+    season_result['expected_purity'] = season_result['spent_fuel'].sub(FUEL_PURITY_VALUE).div(season_result['spent_fuel']).mul(DECREASE_INCREASE_RATIO)
+    season_result['is_leak'] = season_result['is_leak'] & season_result['ratio'].ge(season_result['expected_purity'])
     if is_save_bad_data:
         return (season_result, bad_data)
 
